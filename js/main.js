3c9c3ad62688d4c44fea1d9b600f4cfda9742c52
Shannon Hawn <shannonhawn@hotmail.com>
2016-03-11 16:34:50 -0500

    added functions to the selected variables - forgot to commit

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
 var $header = $('header');
-  $header.addClass('masthead');
-
 var $h1 = $('h1');
-  $h1.removeClass('big-heading');
-  $h1.addClass('logo');
-
 var $main = $('main');
-
-var $p = $('p');
+var $p = $('main p:first-child');
 var $h2 = $('h2');
-var $main = $('main');
-var $img = $('img')
+var $li = $('main li');
+var $img = $('img');
+var $first = $('ul li:first-child');
+var $last = $('ul li:last-child');
 var $dl = $('dl');
 var $dt = $('dt');
+var $show = $('ul li:last-child');
+
+  $header.addClass('masthead');
+  $h1.removeClass('big-heading');
+  $h1.addClass('logo');
+  $main.addClass('wrapper');
+  $p.addClass('intro');
+  $h2.addClass('slug-head');
+  $li.addClass('slug-list');
+  $img.addClass('slug-img');
+  $first.addClass('slug-list-first');
+  $last.addClass('slug-list-last');
+  $dl.addClass('classification');
+  $dt.removeClass('big-label');
+  $show.show('last-child');
